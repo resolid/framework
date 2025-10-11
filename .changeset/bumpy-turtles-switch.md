05de66a50fb03a9fe43f45b78512491c8ec0cd56
Huijie Wei <huijiewei@outlook.com>
2025-10-11 14:29:23 +0800

    chore(di): enable bundle minify and support Node 20.19+

---
+++ .changeset/bumpy-turtles-switch.md
@@ -0,0 +1,5 @@
+---
+"@resolid/di": patch
+---
+
+chore: enable bundle minify and support Node 20.19+
--- packages/di/tsdown.config.ts
+++ packages/di/tsdown.config.ts
@@ -9,4 +9,5 @@ export default defineConfig({
   dts: true,
   treeshake: true,
   clean: true,
+  minify: true,
 }) as UserConfig;
