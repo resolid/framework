3b06441ec3e63994993f39aa64fb6fc95d98b056
Huijie Wei <huijiewei@outlook.com>
2026-03-20 10:08:39 +0800

    chore: update dependencies

--- knip.config.js
+++ knip.config.js
@@ -4,21 +4,11 @@ export default {
       entry: ["src/server.ts", "src/entry.{client,server}.tsx", "src/portals/**/*.tsx"],
       project: "src/**/*.{ts,tsx,css}",
     },
-    "packages/*": {
-      ignore: ["oxlint.config.ts"],
-    },
-    "packages/dev": {
-      ignore: ["oxlint.config.ts"],
-    },
   },
 
   ignoreBinaries: ["only-allow"],
 
-  ignoreDependencies: [
-    "@changesets/cli",
-    "@tailwindcss/language-server",
-    "babel-plugin-react-compiler",
-  ],
+  ignoreDependencies: ["@tailwindcss/language-server", "babel-plugin-react-compiler"],
 
   compilers: {
     css: (text) =>
