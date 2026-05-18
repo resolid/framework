e0b3ab9edbee5dbce45befa3adadebfea8f1e07a
Huijie Wei <huijiewei@outlook.com>
2026-05-18 18:28:39 +0800

    chore: update dependencies

--- packages/cache-redis/vitest.global-setup.ts
+++ packages/cache-redis/vitest.global-setup.ts
@@ -1,5 +1,5 @@
 import { createClient, createCluster } from "@redis/client";
-import { retry } from "@resolid/utils";
+import { retry, timeout } from "@resolid/utils";
 import { execSync } from "node:child_process";
 import { dirname, resolve } from "node:path";
 import { fileURLToPath } from "node:url";
@@ -14,49 +14,45 @@ export default async function (): Promise<() => Promise<void>> {
     stdio: "inherit",
   });
 
-  await retry(
-    async () => {
-      const client = createClient({ url: "redis://localhost:6379" });
+  await Promise.race([
+    retry(
+      async () => {
+        const client = createClient({ url: "redis://localhost:6379" });
 
-      await client.connect();
-      await client.ping();
-      await client.close();
+        await client.connect();
+        await client.ping();
+        await client.close();
 
-      const clusterClient = createCluster({
-        rootNodes: [
-          {
-            url: "redis://localhost:7001",
-          },
-          {
-            url: "redis://localhost:7002",
-          },
-          {
-            url: "redis://localhost:7003",
-          },
-        ],
-        useReplicas: true,
-      });
-      await clusterClient.connect();
+        const clusterClient = createCluster({
+          rootNodes: [
+            {
+              url: "redis://localhost:7001",
+            },
+            {
+              url: "redis://localhost:7002",
+            },
+            {
+              url: "redis://localhost:7003",
+            },
+          ],
+          useReplicas: true,
+        });
+        await clusterClient.connect();
 
-      return clusterClient;
-    },
-    async (clusterClient) => {
-      try {
         const ok = (await clusterClient.clusterInfo()).includes("cluster_state:ok");
+
         await clusterClient.close();
 
-        return ok;
-      } catch {
-        await clusterClient.close();
-        return false;
-      }
-    },
-    200,
-    () => {
-      throw new Error("Redis startup timeout");
-    },
-    10000,
-  );
+        if (!ok) {
+          throw new Error("cluster not ready");
+        }
+      },
+      {
+        delay: 200,
+      },
+    ),
+    timeout(10000),
+  ]);
 
   return async () => {
     execSync(`docker compose -f ${composeFile} --project-name resolid down -v --remove-orphans`, {
